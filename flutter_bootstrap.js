53df26cc130d7fc086b9d6a4df453f9308336aa2
UsmanXperience <uk71719@gmail.com>
2025-09-23 08:09:13 -0700

    deploy: static flutter web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1188185848"
+    serviceWorkerVersion: "3554633780"
   }
 });
